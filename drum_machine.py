d7fcfa78fb074eed47776e55297250ee94d5f08a
ryclorak <ryclorak@gmail.com>
2022-05-25 09:50:11 -0700

    play/pause

--- drum_machine.py
+++ drum_machine.py
@@ -11,6 +11,7 @@ HEIGHT = 800
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
+dark_gray = (50, 50, 50)
 green = (0, 255, 0) # active
 purple = (128, 0, 128) # rim around beat boxes
 blue = (0, 255, 255) # for active beat
@@ -18,6 +19,7 @@ blue = (0, 255, 255) # for active beat
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Drummasheen')
 label_font = pygame.font.Font('FiraSans-Medium.ttf', 32)
+medium_font = pygame.font.Font('FiraSans-Medium.ttf', 24)
 
 fps = 60
 timer = pygame.time.Clock()
@@ -125,6 +127,16 @@ while run:
     screen.fill(black)
     boxes = draw_grid(clicked, active_beat)
 
+    # lower menu buttons
+    play_pause = pygame.draw.rect(screen, gray, [50, HEIGHT-150, 200, 100], 0, 5) # 5 for rounded
+    play_text = label_font.render('Play/Pause', True, white)
+    screen.blit(play_text, (70, HEIGHT-130))
+    if playing:
+        play_text2 = medium_font.render('Playing', True, dark_gray)
+    else:
+        play_text2 = medium_font.render('Paused', True, dark_gray)
+    screen.blit(play_text2, (70, HEIGHT-100))
+
     if beat_changed:
         play_notes()
         beat_changed = False
@@ -139,6 +151,12 @@ while run:
                 if boxes[i][0].collidepoint(event.pos):
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
+        if event.type == pygame.MOUSEBUTTONUP:
+            if play_pause.collidepoint(event.pos):
+                if playing: 
+                    playing = False
+                elif not playing:
+                    playing = True
 
     beat_length = 3600 // bpm # clicks per minute - fps*60
 
